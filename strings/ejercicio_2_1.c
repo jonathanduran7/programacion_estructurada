6f60ca6e39907a0ce7be17c2ef5d8339dc267e8e
jduran <jduran@gen-it.com.ar>
2023-09-10 20:50:32 -0300

    ejercicio 2.1 strings

--- strings/ejercicio_2_1.c
+++ strings/ejercicio_2_1.c
@@ -1,14 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
 
   char frase[500];
   printf("Escriba una frase: ");
-  fgets(frase,500,stdin);
+  fgets(frase, 500, stdin);
 
-  puts(frase);
+  // LeerTextoUnEspacio(frase);
+  LeerTextoMasDeUnEspacio(frase);
 
-  printf("la frase tiene %d palabras", strlen(frase));
   return 0;
+}
+
+void LeerTextoUnEspacio(char frase[])
+{
+  int buscar = 0, contador = 0, palabras = 1;
+  while (buscar == 0)
+  {
+    if (frase[contador] == ' ')
+    {
+      palabras++;
+    }
+
+    if (frase[contador] == '\0')
+    {
+      buscar++;
+    }
+
+    contador++;
+  }
+
+  printf("vueltas: %d \nPalabras: %d", contador, palabras);
+}
+
+void LeerTextoMasDeUnEspacio(char frase[])
+{
+  int valido = 0, contador = 0, palabras = 1, espacios = 0;
+  while (valido == 0)
+  {
+    if (frase[contador] == ' ')
+    {
+      espacios++;
+    }
+    else
+    {
+      if (espacios > 0)
+      {
+        palabras++;
+        espacios = 0;
+      }
+    }
+
+    if (frase[contador] == '\0')
+      valido++;
+
+    contador++;
+  }
+  printf("En la frase hay %d palabras", palabras);
 }
